b99576b2d7a5e166bdf115311319491b32d33a75
[mediba] Jun Doi <j-doi@mediba.jp>
2017-05-16 14:45:42 +0900

    todoモデルの作成(migration前)

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Memo(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    detail = models.TextField()
+    date = models.DateTimeField(defalut=timezone.now)
+    del_flag = models.BooleanField(default=False)
+
+    def delete_memo(self):
+        self.del_flag = True
+        self.save()
+
+    def __str__(self):
+        return self.title
+
