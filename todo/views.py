ed375906c49e079357c83e189ab6582e370f9902
[mediba] Jun Doi <j-doi@mediba.jp>
2017-05-17 14:41:01 +0900

    Topページのビュー作成（一覧表示のみできる状態）

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Memo
 
 # Create your views here.
+
+#トップ画面でTodoの一覧を表示する。
+def todo_list(request):
+    todo_list = get_list_or_404(Memo)
+    print(todo_list)
+    return render(request,'todo/list.html', {'todo_list': todo_list})
