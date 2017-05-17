e7265461325f52190c3caa3f57c4d83707cda875
[mediba] Jun Doi <j-doi@mediba.jp>
2017-05-17 16:52:57 +0900

    新規作成ビューと編集ビューの追加

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.todo_list),
+    url(r'^new$', views.todo_new, name="new"),
     url(r'^detail/(?P<pk>[0-9])+/$', views.todo_detail, name="detail"),
+    url(r'^edit/(?P<pk>[0-9])+/$', views.todo_edit, name="edit"),    
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from .models import Memo
+from .forms import EditForm
+
 
 # Create your views here.
 
@@ -10,8 +12,31 @@ def todo_list(request):
     return render(request,'todo/list.html', {'todo_list': todo_list})
 
 def todo_detail(request, pk):
-    todo = get_object_or_404(Memo, pk=pk)
-    return render(request, 'todo/todo_detail.html', {'todo': todo})
+    memo = get_object_or_404(Memo, pk=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': memo})
 
+def todo_new(request):
+    if request.method == "POST":
+        form = EditForm(request.POST)
+        if form.is_valid():
+            memo = form.save(commit=False)
+            memo.save()
+            return redirect('todo.views.todo_detail', pk=memo.id)
+    else:
+        form = EditForm()        
+    return render(request, 'todo/todo_edit.html', {'form':form})
+
+
+def todo_edit(request, pk):
+    memo = get_object_or_404(Memo, pk=pk)
+    if request.method == "POST":
+        form = EditForm(request.POST, instance=memo)
+        if form.is_valid():
+            memo = form.save(commit=False)
+            memo.save()
+            return redirect('detail', pk=memo.id)
+    else:
+        form = EditForm(instance=memo)
+    return render(request, 'todo/todo_edit.html', {'form': form})
 
 
